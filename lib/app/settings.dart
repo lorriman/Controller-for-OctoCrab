d88887425133479075ae7ed2963bcdd9d83ddd13
lorriman <48987+lorriman@users.noreply.github.com>
2023-05-03 18:59:28 +0100

    always on scrollbar

--- lib/app/settings.dart
+++ lib/app/settings.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:flutter/material.dart';
@@ -28,7 +29,7 @@ class _SettingsViewState extends ConsumerState<SettingsView> {
 
   final Map<ConfigEnum, TextEditingController> _textControllers = {};
   final Map<ConfigEnum, ConfigItem> _configItems = {};
-
+  final _scrollController=ScrollController();
 
   @override
   void initState() {
@@ -37,11 +38,13 @@ class _SettingsViewState extends ConsumerState<SettingsView> {
     _loadConfig(_configItems);
     _initTextControllers(_configItems, _textControllers);
 
+
   }
 
   @override
   void dispose() {
     _disposeTextControllers();
+    _scrollController.dispose();
     if(widget.update!=null) widget.update!();
     super.dispose();
   }
@@ -92,81 +95,83 @@ class _SettingsViewState extends ConsumerState<SettingsView> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView( shrinkWrap : true, children: [
-      if (widget.title!=null)
-        widget.title!
-      else
-        ListTile(title: Text('Configuration',textScaleFactor: 1.4, style: TextStyle(fontWeight: FontWeight.bold)  ,),
-         trailing: Icon(Icons.settings_outlined),
-        ),
-      Divider(),
-      Row(
-        children: [
-          Container(  width : 80,
-            child: IconButton(
-              style: ButtonStyle(elevation: MaterialStateProperty.all(20.0),
-                  shadowColor: MaterialStateProperty.all(Colors.red)),
-              icon:
-              Icon(Icons.power_settings_new, color: Colors.red.shade300),
-              onPressed: widget.onShutdown,
-              iconSize: 50,
-              tooltip: 'shutdown device',
-            ),
+    return CupertinoScrollbar(thumbVisibility: true, controller: _scrollController,thickness: 10,
+      child: ListView( shrinkWrap : true,controller: _scrollController,children: [
+        if (widget.title!=null)
+          widget.title!
+        else
+          ListTile(title: Text('Configuration',textScaleFactor: 1.4, style: TextStyle(fontWeight: FontWeight.bold)  ,),
+           trailing: Icon(Icons.settings_outlined),
           ),
-          Text('shutdown remote device',textScaleFactor: 1.2, style: TextStyle(color: Colors.red)),
-        ],
-      ),
-      Divider(),
-      for (final item in _configItems.values.where((e)=>e.itemEnum.enabled))
-            (){
+        Divider(),
+        Row(
+          children: [
+            Container(  width : 80,
+              child: IconButton(
+                style: ButtonStyle(elevation: MaterialStateProperty.all(20.0),
+                    shadowColor: MaterialStateProperty.all(Colors.red)),
+                icon:
+                Icon(Icons.power_settings_new, color: Colors.red.shade300),
+                onPressed: widget.onShutdown,
+                iconSize: 50,
+                tooltip: 'shutdown device',
+              ),
+            ),
+            Text('shutdown remote device',textScaleFactor: 1.2, style: TextStyle(color: Colors.red)),
+          ],
+        ),
+        Divider(),
+        for (final item in _configItems.values.where((e)=>e.itemEnum.enabled))
+              (){
 
-          final sharedPreferencesService =
-          ref.read(sharedPreferencesServiceProvider);
+            final sharedPreferencesService =
+            ref.read(sharedPreferencesServiceProvider);
 
 
-          if (item.itemEnum.checkbox){
-            final value=sharedPreferencesService.sharedPreferences
-                .getString(item.itemEnum.key) ?? 'false';
-            return Row(
-              children: [
-                Checkbox(value:value=='true',onChanged: true ? null : (value){
+            if (item.itemEnum.checkbox){
+              final value=sharedPreferencesService.sharedPreferences
+                  .getString(item.itemEnum.key) ?? 'false';
+              return Row(
+                children: [
+                  Checkbox(value:value=='true',onChanged: true ? null : (value){
 
+                    sharedPreferencesService.sharedPreferences
+                        .setString(item.itemEnum.key, value! ? 'true' : 'false' );
+                  }
+                    , ),
+                  Text(item.itemEnum.label+' (tba)',style: TextStyle(color: Colors.grey)),
+                ],
+
+              );
+            }
+
+            return InputBox(
+              item.itemEnum.label,
+              _textControllers[item.itemEnum]!,
+              sharedPrefKey: item.itemEnum.key,
+              ref: ref,
+              password: item.itemEnum.key == sharedPrefKey_password,
+              onChanged: (value) {
+                //brute-forcing the update because the Drawer doesn't have events like onUnShow
+                // which means the c1-10 custom functions don't appear/disappear as they should
+                //todo: optimise the setState
+                setState((){
+                  String str = value.trim();
+                  if (str.length > 2040) {
+                    /* max url length =2048, we subtract a few bytes */
+                    str = str.substring(1, 2040);
+                  }
                   sharedPreferencesService.sharedPreferences
-                      .setString(item.itemEnum.key, value! ? 'true' : 'false' );
-                }
-                  , ),
-                Text(item.itemEnum.label+' (tba)',style: TextStyle(color: Colors.grey)),
-              ],
+                      .setString(item.itemEnum.key, str);
+                  _configItems[item.itemEnum] =
+                      ConfigItem(item.itemEnum, str);
+                  _configureApi(_configItems);
+                });
+              },
+            ); }(),
 
-            );
-          }
-
-          return InputBox(
-            item.itemEnum.label,
-            _textControllers[item.itemEnum]!,
-            sharedPrefKey: item.itemEnum.key,
-            ref: ref,
-            password: item.itemEnum.key == sharedPrefKey_password,
-            onChanged: (value) {
-              //brute-forcing the update because the Drawer doesn't have events like onUnShow
-              // which means the c1-10 custom functions don't appear/disappear as they should
-              //todo: optimise the setState
-              setState((){
-                String str = value.trim();
-                if (str.length > 2040) {
-                  /* max url length =2048, we subtract a few bytes */
-                  str = str.substring(1, 2040);
-                }
-                sharedPreferencesService.sharedPreferences
-                    .setString(item.itemEnum.key, str);
-                _configItems[item.itemEnum] =
-                    ConfigItem(item.itemEnum, str);
-                _configureApi(_configItems);
-              });
-            },
-          ); }(),
-
-    ]);
+      ]),
+    );
   }
 
 
